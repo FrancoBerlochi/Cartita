13e4373a80b65de667b430713ce5616099345409
Franco Berlochi <francoberlochi@gmail.com>
2026-01-28 15:30:42 -0300

    #

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -52,7 +52,7 @@ export function Header() {
             stroke-linecap="round"
             stroke-linejoin="round"
             stroke-width="2"
-            class="lucide lucide-menu-icon lucide-menu"
+            className="lucide lucide-menu-icon lucide-menu"
             viewBox="0 0 24 24"
           >
             <path d="M4 5h16M4 12h16M4 19h16" />
@@ -85,7 +85,7 @@ export function Header() {
                 stroke-width="2"
                 stroke-linecap="round"
                 stroke-linejoin="round"
-                class="lucide lucide-menu-icon lucide-menu"
+                className="lucide lucide-menu-icon lucide-menu"
               >
                 <path d="M4 5h16" />
                 <path d="M4 12h16" />
