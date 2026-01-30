039df0da0b3f8c46f48b07043b24dd2a6901d4b3
Franco Berlochi <francoberlochi@gmail.com>
2026-01-30 19:08:39 -0300

    #

--- components/sections/hero-section.tsx
+++ components/sections/hero-section.tsx
@@ -11,7 +11,7 @@ const socialLinks = [
 
 export function HeroSection() {
   return (
-    <section className="px-4 sm:px-6 lg:px-16 xl:px-24 py-16">
+    <section className="px-4 sm:px-6 lg:px-16 xl:px-24 py-16 max-md:py-4">
       <div className="max-w-7xl mx-auto">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-14 items-center">
           {/* TEXTO */}
